05f5ac79d6047ddd0fce6a86a0467dec3392ba26
Vladimir Klykov <MarkLouren@users.noreply.github.com>
2019-03-11 12:40:13 +0200

    Update 25-TelegramBot-Beginner-PublicApi.py

--- 25-TelegramBot-Beginner-PublicApi.py
+++ 25-TelegramBot-Beginner-PublicApi.py
@@ -1,5 +1,4 @@
 # use Liabrary: https://github.com/python-telegram-bot/python-telegram-bot
-
 from telegram.ext import Updater, CommandHandler
 import requests
 import re
@@ -12,17 +11,25 @@ def get_url():
 # send an image
 # chat_id = update.message.chat_id
 
+def get_image_url():  # chose only jpg in urls, because telegram not supports Gif
+    allowed_extension = ['jpg','jpeg','png']
+    file_extension = ''
+    while file_extension not in allowed_extension:
+        url = get_url()
+        file_extension = re.search("([^.]*)$",url).group(1).lower()
+    return url
+
 def dog(bot, update):
-    url =get_url() #from the previous function
+    url =get_image_url() #from the previous function
     chat_id = update.message.chat_id
     bot.send_photo(chat_id=chat_id, photo=url)
 
 def main():
-    updater = Updater('tokenId') #bot token
-    db = updater.dispatcher  #send a message
-    db.add_handler(CommandHandler('dog', dog))#command Dog
+    updater = Updater('tokenid') #bot token
+    db = updater.dispatcher  #send message
+    db.add_handler(CommandHandler('dog', dog))
     updater.start_polling()
     updater.idle()
 
-if __name__ == '__main__':   #launch bot
+if __name__ == '__main__':
     main()
