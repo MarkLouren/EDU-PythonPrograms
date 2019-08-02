4c6e820eb8498c8f8cae7110613c408781944905
Vladimir Klykov <MarkLouren@users.noreply.github.com>
2019-08-02 15:05:20 +0300

    Update 29-Udemy-CreateClass.py

--- 29-Udemy-CreateClass.py
+++ 29-Udemy-CreateClass.py
@@ -49,3 +49,21 @@ class Dog(Animal):
 
 d=Dog('dogname')
 print (d.fetch('meat'))
+
+#Multiple inheritance
+
+class A(object):
+    def dothis(self):
+        print ('doing this in A')
+
+class B(A):
+    pass
+
+class C(object):
+    def dothis(self):
+        print ('doing this in C')
+
+class D(B,C): #multiple inheritance
+    pass
+
+print (D.mro()) #order of class inheritance B,A,C
